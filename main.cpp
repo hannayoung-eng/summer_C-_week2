9fb9eddac8049c8a6f4cd0f4145d03b568c41dc2
hannayoung-eng <hannayoung@ewha.ac.kr>
2026-07-06 02:51:11 +0000

    week2-1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "alarm.h"
+#include "timeOfDay.h"
 #include <iostream>
 
 namespace hannayoung2693327
@@ -12,11 +13,24 @@ namespace hannayoung2693327
 int main()
     {
         using namespace hannayoung2693327;
-        alarm a1;
-        a1.print();
-        alarm a2{timeOfDay{11,44},1};
-        a2.print();
-        if (compareTimeOfDay(a1.getWakeTime(),a2.getWakeTime()))
-        std::cout<<"same\n";
-        else std::cout<<"different\n";
+        timeOfDay t1, t2;
+        std::cin >>t1>>t2;
+        std::cout <<t1<<" "<<t2<<'\n';
+        std::cout << ++t2 <<'\n';
+        std::cout << t2++<<'\n';
+        std::cout <<t2 <<'\n';
+
+        if (t1==t2) std::cout << "same\n";
+        else std::cout <<"different\n";
+        std::cout << t1+t2<<std::endl;
+
+        return 0;
+
+        // alarm a1;
+        // a1.print();
+        // alarm a2{timeOfDay{11,44},1};
+        // a2.print();
+        // if (compareTimeOfDay(a1.getWakeTime(),a2.getWakeTime()))
+        // std::cout<<"same\n";
+        // else std::cout<<"different\n";
     }
--- timeOfDay.h
+++ timeOfDay.h
@@ -51,6 +51,50 @@ namespace hannayoung2693327
         int getHour() const {return hour;}
         int getMinute() const {return minute;}
 
+        timeOfDay operator++()
+        {
+            ++minute; //0-59
+            if (minute ==60) {minute =0; ++hour;}//0-23
+            if (hour ==24) {hour=0; }
+            return timeOfDay {hour, minute};
+        }
+        timeOfDay operator++(int)
+        {
+            timeOfDay temp(hour, minute);
+            ++minute; //0-59
+            if (minute ==60) {minute =0; ++hour;}//0-23
+            if (hour ==24) {hour=0; }
+            return temp;
+        }
+        friend std::istream& operator >>(std::istream& is, timeOfDay& t)
+        {
+            // std::cin --> is
+            std::cout<<"Enter the hour(0~23) as a number : "; is >> t.hour; t.testHour();
+            std::cout<<"ENter the minute(0~59) as a number : "; is >> t.minute; t.testMinute();
+            return is;
+        }
+        friend std::ostream& operator<<(std::ostream& os, const timeOfDay& t)
+        {
+            //std::cout -->os
+            if (t.hour<10) os<<"0"; os << t.hour<<":";
+            if (t.minute<10) os <<"0"; os <<t.minute;
+            return os;
+        }
+        friend bool operator==(const timeOfDay& t1, const timeOfDay& t2)
+        {
+            return t1.hour ==t2.hour && t1.minute == t2.minute;
+        }
+        friend timeOfDay operator+(const timeOfDay& t1, const timeOfDay& t2)
+        {
+            int minute1{t1.minute+t1.hour*60};
+            int minute2{t2.minute+t2.minute*60};
+            int totalMinute{minute1+minute2};
+            
+            int newMinute{totalMinute%60};
+            int newHour{totalMinute /60};
+            if(newHour >24) newHour %= 24 ;
+            return timeOfDay{newHour, newMinute};
+        }
     };
 
 }
