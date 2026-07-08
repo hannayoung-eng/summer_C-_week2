9baf8192f4313a30614dc796ae443797155bd090
hannayoung-eng <hannayoung@ewha.ac.kr>
2026-07-08 01:33:30 +0000

    lab6

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
 #include "alarm.h"
-#include "timeOfDay.h"
-#include <iostream>
-
+#include <array>
 namespace hannayoung2693327
 {
-    bool compareTimeOfDay(const timeOfDay& t1, const timeOfDay& t2)
+    void printAlarmArray(const alarm a[], const int n)
     {
-        return t1.getHour()==t2.getHour() && t1.getMinute()==t2.getMinute();
+        for(int i =0; i<n;++i)
+        std::cout <<a[i];
     }
 }
 
 int main()
     {
         using namespace hannayoung2693327;
-        timeOfDay t1, t2;
-        std::cin >>t1>>t2;
-        std::cout <<t1<<" "<<t2<<'\n';
-        std::cout << ++t2 <<'\n';
-        std::cout << t2++<<'\n';
-        std::cout <<t2 <<'\n';
+        const int n{4};
+        alarm a[n];
+        a[0]=alarm{"morning", {5,30},1};
+        a[1].setName("class");
+        a[1].setWakeTime({9,0});
+        a[1].setActive(1);
+        a[1].print();
+        // a[2].input();
+        // a[2].print();
+        std::cin >>a[3];
+        printAlarmArray(a,n);
+        std::array<alarm, n> b;
+        for (int i =0;i<b.size();++i)
+            b.at(i) = a[1];
+        for (const auto& bi : b)
+            bi.print();
+        // timeOfDay t1, t2;
+        // std::cin >>t1>>t2;
+        // std::cout <<t1<<" "<<t2<<'\n';
+        // std::cout << ++t2 <<'\n';
+        // std::cout << t2++<<'\n';
+        // std::cout <<t2 <<'\n';
 
-        if (t1==t2) std::cout << "same\n";
-        else std::cout <<"different\n";
-        std::cout << t1+t2<<std::endl;
-
+        // if (t1==t2) std::cout << "same\n";
+        // else std::cout <<"different\n";
+        // std::cout << t1+t2<<std::endl;
         return 0;
 
         // alarm a1;
